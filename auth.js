a29934a9ec0335da876c6f6ae7ab32a016c6f2d6
Bin Wang <wbin00@gmail.com>
2016-08-30 17:03:15 +0800

    reddit login

---
+++ auth.js
@@ -0,0 +1,50 @@
+
+request = require('request')
+
+const clientId = 'YKa6s8nWTInbhQ'
+const redirectUrl = 'view://reddit_redirect.html'
+const scope = 'identity'
+const baseUrl = 'https://www.reddit.com/api/v1'
+
+
+function requestToken(code, cb) {
+  const tokenUrl = `${baseUrl}/access_token`
+  console.log(tokenUrl)
+  const opts = {
+    url: tokenUrl,
+    method: 'POST',
+    json: true,
+    auth: {
+      user: clientId,
+      pass: ''
+    },
+    form: {
+      grant_type: 'authorization_code',
+      'code': code,
+      'redirect_uri': redirectUrl
+    }
+  }
+  request(opts, function(err, res, body) {
+    console.log(body.access_token)
+    cb(undefined, body.access_token)
+  })
+}
+
+function getToken(win, cb) {
+  const authUrl = `${baseUrl}/authorize?client_id=${clientId}&response_type=code&state=RANDOM_STRI}&redirect_uri=${redirectUrl}&scope=${scope}`
+  win.loadURL(authUrl)
+
+  win.webContents.on('did-get-redirect-request', (event, oldUrl, newUrl, isMainFrame,
+        httpResponseCode, requestMethod, referrer, headers) => {
+    if (newUrl.startsWith(redirectUrl)) {
+      console.log(newUrl)
+      const codeRe = /code=([\w-]+)/
+      const code = newUrl.match(codeRe)[1]
+      console.log(code)
+      console.log(cb)
+      requestToken(code, cb)
+    }
+  })
+}
+
+module.exports.getToken = getToken
---
+++ main.js
@@ -0,0 +1,51 @@
+
+const {app, protocol, BrowserWindow, webContents} = require('electron')
+const path = require('path')
+
+const {getToken} = require('./auth')
+
+let mainWindow
+
+function registerProtocol() {
+  protocol.registerFileProtocol('view', (request, callback) => {
+    const url = request.url.substr(7)
+    callback({path: path.normalize(`${__dirname}/${url}`)})
+  }, (error) => {
+    if (error) console.error('Failed to register protocol')
+  })
+}
+
+function createWindow () {
+  registerProtocol()
+
+  mainWindow = new BrowserWindow()
+  getToken(mainWindow, function (err, token) {
+    mainWindow.loadURL('view://index.html')
+    mainWindow.webContents.on('did-finish-load', () => {
+      mainWindow.webContents.send('reddit-token', token)
+    })
+  })
+  // mainWindow.loadURL(`file://${__dirname}/index.html?a=b`)
+  // mainWindow.loadURL("view://index.html")
+  // mainWindow.loadURL("view://reddit_redirect.html")
+
+  mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
